8d2b0d98245a906b2a5258f1d19c954ba3caf7ea
Jade Philipoom <jadep@google.com>
2022-11-10 21:34:51 +0100

    attempt to write a version of randombytes that will work on OT

--- sw/device/sphincsplus/randombytes.c
+++ sw/device/sphincsplus/randombytes.c
@@ -2,43 +2,73 @@
 This code was taken from the SPHINCS reference implementation and is public domain.
 */
 
-#include <fcntl.h>
-#include <unistd.h>
-
 #include "randombytes.h"
 
-static int fd = -1;
+#include "sw/device/lib/dif/dif_rv_core_ibex.h"
+#include "sw/device/lib/runtime/ibex.h"
+
+// N.B. As currently implemented, `abort()` will just wait for an interrupt and
+// eventually time out.
+#define CHECK(cond) \
+  do {              \
+    if (!cond) {    \
+      abort();      \
+    }               \
+  } while (false)
+
+/* Adapted from rv_core_ibx_testutils.c */
+static bool is_rnd_data_valid(const dif_rv_core_ibex_t *rv_core_ibex) {
+  dif_rv_core_ibex_rnd_status_t rnd_status;
+  CHECK(dif_rv_core_ibex_get_rnd_status(rv_core_ibex, &rnd_status) == kDifOk);
+  return rnd_status & kDifRvCoreIbexRndStatusValid;
+}
+
+/* Adapted from rv_core_ibx_testutils.c */
+static void read_rnd_data(const dif_rv_core_ibex_t *rv_core_ibex, uint32_t *rnd_data) {
+  while (!is_rnd_data_valid(rv_core_ibex)) {
+    // Spin until rnd is valid.
+  }
+
+  CHECK(dif_rv_core_ibex_read_rnd_data(rv_core_ibex, rnd_data) == kDifOk);
+}
 
 void randombytes(unsigned char *x, unsigned long xlen)
 {
-    unsigned long i;
-    ssize_t r;
 
-    if (fd == -1) {
-        for (;;) {
-            fd = open("/dev/urandom", O_RDONLY);
-            if (fd != -1) {
-                break;
-            }
-            sleep(1);
-        }
+    // Initialize Ibex. It is OK to do this several times (it just records the
+    // base address passed to it).
+    dif_rv_core_ibex_t rv_core_ibex;
+    CHECK_DIF_OK(dif_rv_core_ibex_init(
+          mmio_region_from_addr(TOP_EARLGREY_RV_CORE_IBEX_CFG_BASE_ADDR),
+          &rv_core_ibex));
+
+    // If x is not word-aligned, write some random bytes until it is.
+    size_t offset = x % sizeof(uint32_t);
+    if (offset != 0) {
+      uint32_t rnd_data;
+      read_rnd_data(&rv_core_ibex, &rnd_data);
+      for (size_t i = 0; i < sizeof(uint32_t) - offset; i++) {
+        x[i] = (uint8_t) (rnd_data & 255);
+        rnd_data >>= 8;
+        xlen--;
+      }
     }
 
-    while (xlen > 0) {
-        if (xlen < 1048576) {
-            i = xlen;
-        }
-        else {
-            i = 1048576;
-        }
+    size_t nwords = xlen / sizeof(uint32_t);
+    for (size_t i = 0; i < nwords; i++) {
+      // Read 32 bits of randomness and store in the result buffer.
+      read_rnd_data(&rv_core_ibex, x);
+      xlen -= sizeof(uint32_t);
+    }
 
-        r = read(fd, x, i);
-        if (r < 1) {
-            sleep(1);
-            continue;
-        }
-
-        x += r;
-        xlen -= r;
+    if (xlen > 0) {
+      // There is a non-full-word amount of bytes remaining; write these
+      // byte-by-byte.
+      uint32_t rnd_data;
+      read_rnd_data(&rv_core_ibex, &rnd_data);
+      for (size_t i = 0; i < sizeof(uint32_t) - offset; i++) {
+        x[i] = (uint8_t) (rnd_data & 255);
+        rnd_data >>= 8;
+      }
     }
 }
