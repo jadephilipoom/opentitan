0718745832a8923302b3bfb3a979ea94584bc99b
Jade Philipoom <jadep@google.com>
2022-11-11 12:32:37 +0100

    add some printing to see progress

--- sw/device/sphincsplus/verify.c
+++ sw/device/sphincsplus/verify.c
@@ -5,6 +5,7 @@
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/crypto/drivers/entropy.h"
 #include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/entropy_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
@@ -18,26 +19,30 @@ OTTF_DEFINE_TEST_CONFIG();
 bool test_main() {
   unsigned char mout[SPX_SMLEN];
   unsigned long long mlen;
-  bool ret = 0;
+
+  LOG_INFO("Starting SPHICS+ verify test...");
 
   // Initialize the CSRNG (using only TRNG, no sw seed material).
   entropy_seed_material_t empty_seed = {
     .len = 0,
     .data = {0},
   };
+  entropy_testutils_auto_mode_init();
   status_t status = entropy_csrng_instantiate(kHardenedBoolFalse,
       &empty_seed);
   CHECK(status_ok(status));
 
-  /* Test if signature is valid. */
+  LOG_INFO("CSRNG initialized/instantiated successfully.");
+
+  // Test if signature is valid.
   CHECK(crypto_sign_open(mout, &mlen, sm, SPX_SMLEN, pk) == 0, "  X verification failed!");
   LOG_INFO("    verification succeeded.");
 
-  /* Test if the correct message was recovered. */
+  // Test if the correct message was recovered.
   CHECK(mlen == SPX_MLEN, "  X mlen incorrect [%llu != %u]!\n", mlen, SPX_MLEN);
   LOG_INFO("    mlen as expected [%llu].", mlen);
   CHECK(memcmp(m, mout, SPX_MLEN) == 0, "  X output message incorrect!");
   LOG_INFO("    output message as expected.");
 
-  return ret;
+  return true;
 }
