a4b0006e41dc12591405759742cb65605a2f4463
Jade Philipoom <jadep@google.com>
2022-11-15 21:03:33 +0100

    force alignment of WOTS pk to avoid copies in thash

--- sw/device/sphincsplus/sign.c
+++ sw/device/sphincsplus/sign.c
@@ -57,7 +57,7 @@ int crypto_sign_verify(const uint8_t *sig, size_t siglen,
     spx_ctx ctx;
     const unsigned char *pub_root = pk + SPX_N;
     unsigned char mhash[SPX_FORS_MSG_BYTES];
-    unsigned char wots_pk[SPX_WOTS_BYTES];
+    unsigned char wots_pk[SPX_WOTS_BYTES] __attribute__((aligned(sizeof(uint32_t))));
     unsigned char root[SPX_N];
     unsigned char leaf[SPX_N];
     unsigned int i;
