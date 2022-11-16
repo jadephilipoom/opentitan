1f909d064dd495ec7e2c6bf64042a2342dbe1995
Jade Philipoom <jadep@google.com>
2022-11-16 13:30:42 +0100

    remove profiling stuff from thash

--- sw/device/sphincsplus/thash_shake_simple.c
+++ sw/device/sphincsplus/thash_shake_simple.c
@@ -6,26 +6,8 @@
 #include "address.h"
 #include "params.h"
 
-#include "sw/device/lib/runtime/ibex.h" // TODO: remove, for profiling
-#include "sw/device/lib/runtime/log.h" // TODO: remove, for profiling
 #include "fips202.h"
 
-
-/**
- * Start a cycle-count timing profile.
- */
-static uint64_t profile_start() { return ibex_mcycle_read(); }
-
-/**
- * End a cycle-count timing profile.
- *
- * Call `profile_start()` first.
- */
-static uint32_t profile_end(uint64_t t_start) {
-  uint64_t t_end = ibex_mcycle_read();
-  uint64_t cycles = t_end - t_start;
-  return (uint32_t)cycles;
-}
 /**
  * Takes an array of inblocks concatenated arrays of SPX_N bytes.
  */
