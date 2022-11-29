44945bcb05415a2e7e371e10ce6558b4c45cef2f
Jade Philipoom <jadep@google.com>
2022-11-29 13:59:41 +0100

    with icache disabled: Verilator 20, FPGA 28

--- sw/device/tests/timing_test.c
+++ sw/device/tests/timing_test.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
+#include "sw/device/lib/base/bitfield.h"
+#include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/runtime/ibex.h"
 #include "sw/device/lib/runtime/print.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
@@ -32,7 +34,19 @@ static __attribute__((noinline)) void print_cycles(const char *name, const uint3
   base_printf("  %s took %u cycles\n", name, cycles);
 }
 
+static __attribute__((noinline)) void disable_icache(void) {
+  uint32_t cpuctrl;
+  CSR_READ(CSR_REG_CPUCTRL, &cpuctrl);
+  CSR_WRITE(CSR_REG_CPUCTRL, 0x7C0);
+  // Instruction cache enable is bit 0 of CPUCTRL CSR:
+  // https://ibex-core.readthedocs.io/en/latest/03_reference/cs_registers.html
+  cpuctrl = bitfield_bit32_write(cpuctrl, 0, 0);
+  CSR_WRITE(CSR_REG_CPUCTRL, cpuctrl);
+}
+
 bool test_main(void) {
+  disable_icache();
+
   uint64_t t_start = profile_start();
   uint32_t cycles = profile_end(t_start);
 
