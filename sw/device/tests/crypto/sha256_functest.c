6617062bb0937ab0475590dd67e9f6a2dfa467ea
Jade Philipoom <jadep@google.com>
2023-09-08 15:11:43 +0200

    wip, got benchmarks for sha256

--- sw/device/tests/crypto/sha256_functest.c
+++ sw/device/tests/crypto/sha256_functest.c
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "sw/device/lib/crypto/drivers/hmac.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
 #include "sw/device/lib/crypto/include/hash.h"
 #include "sw/device/lib/runtime/log.h"
@@ -49,6 +48,13 @@ static const uint8_t kExactBlockExpDigest[] = {
     0x60, 0x3a, 0x8c, 0x44, 0x7c, 0xd1, 0xd9, 0x15, 0x11, 0x08,
 };
 
+enum {
+  /**
+   * Number of 32-bit words in a SHA-256 digest.
+   */
+  kDigestNumWords = 256 / 32,
+};
+
 /**
  * Call the `otcrypto_hash` API and check the resulting digest.
  *
@@ -57,15 +63,15 @@ static const uint8_t kExactBlockExpDigest[] = {
  */
 static status_t run_test(crypto_const_byte_buf_t msg,
                          const uint32_t *exp_digest) {
-  uint32_t act_digest[kHmacDigestNumWords];
+  uint32_t act_digest[kDigestNumWords];
   crypto_word32_buf_t digest_buf = {
       .data = act_digest,
-      .len = kHmacDigestNumWords,
+      .len = kDigestNumWords,
   };
   uint64_t t_start = profile_start();
   TRY(otcrypto_hash(msg, kHashModeSha256, &digest_buf));
   profile_end_and_print(t_start, "sha256");
-  TRY_CHECK_ARRAYS_EQ(act_digest, exp_digest, kHmacDigestNumWords);
+  TRY_CHECK_ARRAYS_EQ(act_digest, exp_digest, kDigestNumWords);
   return OK_STATUS();
 }
 
@@ -191,41 +197,44 @@ static status_t long_test(void) {
  * Test streaming API with a long message.
  *
  * SHA256(kRandomDataOneKilobyte || kRandomDataOneKilobyte || kRandomDataOneKilobyte)
- *   = 0x655dd1aaf27aa5f252d767704a873338be66aca295c0bf8c204b7255be9af9cd
+ *   = 0x6a53a80bced7e3928e7f1607176553f9113e15ac54ed06cf82bd865917940282
  */
-/*
-static status_t multiple_update_streaming_test(void) {
-  hash_context_t ctx;
-  TRY(otcrypto_hash_init(&ctx, kHashModeSha256));
+static status_t long_streaming_test(void) {
+  const uint32_t exp_digest[] = {
+      0x0ba8536a,
+      0x92e3d7ce,
+      0x07167f8e,
+      0xf9536517,
+      0xac153e11,
+      0xcf06ed54,
+      0x5986bd82,
+      0x82029417,
+  };
+  // Construct message buffer, which will be chained with itself.
+  crypto_const_byte_buf_t msg_buf = {
+      .data = (unsigned char *)kRandomDataOneKilobyte,
+      .len = sizeof(kRandomDataOneKilobyte),
+  };
 
-  // Send 0 bytes, then 1, then 2, etc. until message is done.
-  const unsigned char *next = kTwoBlockMessage;
-  size_t len = kTwoBlockMessageLen;
-  size_t update_size = 0;
-  while (len > 0) {
-    update_size = len <= update_size ? len : update_size;
-    crypto_const_byte_buf_t msg_buf = {
-        .data = next,
-        .len = update_size,
-    };
-    next += update_size;
-    len -= update_size;
-    update_size++;
-    TRY(otcrypto_hash_update(&ctx, msg_buf));
-  }
-  size_t digest_num_words =
-      (sizeof(kTwoBlockExpDigest) + sizeof(uint32_t) - 1) / sizeof(uint32_t);
-  uint32_t act_digest[digest_num_words];
+  // Prepare a buffer to store the computed digest.
+  uint32_t act_digest[kDigestNumWords];
   crypto_word32_buf_t digest_buf = {
       .data = act_digest,
-      .len = digest_num_words,
+      .len = kDigestNumWords,
   };
+
+  uint64_t t_start = profile_start();
+  hash_context_t ctx;
+  TRY(otcrypto_hash_init(&ctx, kHashModeSha256));
+  for (size_t i = 0; i < 3; i++) {
+    TRY(otcrypto_hash_update(&ctx, msg_buf));
+  }
   TRY(otcrypto_hash_final(&ctx, &digest_buf));
-  TRY_CHECK_ARRAYS_EQ((unsigned char *)act_digest, kTwoBlockExpDigest,
-                      sizeof(kTwoBlockExpDigest));
+  profile_end_and_print(t_start, "sha256 3kB streaming");
+
+  TRY_CHECK_ARRAYS_EQ(act_digest, exp_digest, kDigestNumWords);
   return OK_STATUS();
 }
-*/
 
 OTTF_DEFINE_TEST_CONFIG();
 
@@ -236,6 +245,6 @@ bool test_main(void) {
   EXECUTE_TEST(test_result, one_update_streaming_test);
   EXECUTE_TEST(test_result, multiple_update_streaming_test);
   EXECUTE_TEST(test_result, long_test);
-  // EXECUTE_TEST(test_result, long_streaming_test);
+  EXECUTE_TEST(test_result, long_streaming_test);
   return status_ok(test_result);
 }
