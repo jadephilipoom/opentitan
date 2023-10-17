3c25af3d25eec9ab491830925e7ff6aed29a2b2a
Moritz Wettermann <moritz.wettermann@gi-de.com>
2023-10-17 14:29:27 +0200

    [sw/crypto] Add check if public key is valid to P-384 ECDH
    This commit
    - separates p384_isoncurve from p384_verify.s file into an own file in
    order to be able to use it for other functions beside verify.
    - adds a function to the p384_shared_key.s binary to check if a provided
    public key is valid.
    
    !!!! Attention !!!!
    This additions cause the p384_shared_key.s binary to not compile
    anymore, because it exceeds IMEM boundaries. A solution for this still
    has to be found.
    
    Signed-off-by: Moritz Wettermann <moritz.wettermann@gi-de.com>

--- sw/otbn/crypto/p384_ecdh_shared_key.s
+++ sw/otbn/crypto/p384_ecdh_shared_key.s
@@ -64,6 +64,12 @@ start:
  * clobbered flag groups: FG0
  */
 shared_key:
+  /* Validate the public key. Halts the program if the key is invalid and jumps
+     back here if it's OK. */
+  jal       x0, check_public_key_valid
+
+  _pk_valid:
+
   /* Generate arithmetically masked shared key d*Q.
        dmem[x] <= (d*Q).x - m_x mod p
        dmem[y] <= m_x */
@@ -90,6 +96,130 @@ shared_key:
 
   ecall
 
+/**
+ * Check if a provided public key is valid.
+ *
+ * For a given public key (x, y), check that:
+ * - x and y are both fully reduced mod p
+ * - (x, y) is on the P-384 curve.
+ *
+ * Note that, because the point is in affine form, it is not possible that (x,
+ * y) is the point at infinity. In some other forms such as projective
+ * coordinates, we would need to check for this also.
+ *
+ * This routine raises a software error and halts operation if the public key
+ * is invalid.
+ *
+ * @param[in]  dmem[12]: dptr_r, pointer to dmem location where right
+ *                               side result r will be stored
+ * @param[in]  dmem[16]: dptr_s, pointer to dmem location where left side
+ *                               result s will be stored
+ * @param[in]  dmem[20]: dptr_x, pointer to dmem location containing affine
+ *                               x-coordinate of input point
+ * @param[in]  dmem[24]: dptr_y, pointer to dmem location containing affine
+ *                               y-coordinate of input point
+ *
+ * Flags: Flags have no meaning beyond the scope of this subroutine.
+ *
+ * clobbered registers: x2, x3, x20 to x23, w0 to w17
+ * clobbered flag groups: FG0
+ */
+check_public_key_valid:
+  /* Init all-zero register. */
+  bn.xor    w31, w31, w31
+
+  /* load domain parameter p (modulus)
+     [w13, w12] = p = dmem[p384_p] */
+  li        x2, 12
+  la        x3, p384_p
+  bn.lid    x2++, 0(x3)
+  bn.lid    x2++, 32(x3)
+
+  /* Load public key x-coordinate.
+     [w11, w10] <= dmem[x] = x */
+  la        x20, dptr_x
+  lw        x20, 0(x20)
+  li        x2, 10
+  bn.lid    x2++, 0(x20)
+  bn.lid    x2, 32(x20)
+
+  /* Compare x to p.
+       FG0.C <= (x < p) */
+  bn.sub    w0, w10, w12
+  bn.subb   w0, w11, w13
+
+  /* Trigger a fault if FG0.C is false. */
+  csrrs     x2, 0x7c0, x0
+  andi      x2, x2, 1
+  bne       x2, x0, _x_valid
+  unimp
+
+  _x_valid:
+
+  /* Load public key y-coordinate.
+       w2 <= dmem[y] = y */
+  la        x21, dptr_y
+  lw        x21, 0(x21)
+  li        x2, 8
+  bn.lid    x2++, 0(x21)
+  bn.lid    x2, 32(x21)
+
+  /* Compare y to p.
+       FG0.C <= (y < p) */
+  bn.sub    w0, w8, w12
+  bn.subb   w0, w9, w13
+
+  /* Trigger a fault if FG0.C is false. */
+  csrrs     x2, 0x7c0, x0
+  andi      x2, x2, 1
+  bne       x2, x0, _y_valid
+  unimp
+
+  _y_valid:
+
+  /* Compute both sides of the Weierstrauss equation.
+       dmem[r] <= (x^3 + ax + b) mod p
+       dmem[s] <= (y^2) mod p */
+  jal       x1, p384_isoncurve
+
+  /* Load both sides of the equation.
+       [w7, w6] <= dmem[r]
+       [w5, w4] <= dmem[s] */
+  la        x22, dptr_r
+  lw        x22, 0(x22)
+  li        x2, 6
+  bn.lid    x2++, 0(x22)
+  bn.lid    x2, 32(x22)
+  la        x23, dptr_s
+  lw        x23, 0(x23)
+  li        x2, 4
+  bn.lid    x2++, 0(x23)
+  bn.lid    x2, 32(x23)
+
+  /* Compare the two sides of the equation.
+       FG0.Z <= (y^2) mod p == (x^2 + ax + b) mod p */
+  bn.sub    w0, w4, w6
+  bn.subb   w1, w5, w7
+
+  bn.cmp    w0, w31
+
+  /* Trigger a fault if FG0.Z is false. */
+  csrrs     x2, 0x7c0, x0
+  srli      x2, x2, 3
+  andi      x2, x2, 1
+  bne       x2, x0, _pk_1st_reg_valid
+  unimp
+
+  _pk_1st_reg_valid:
+
+  bn.cmp    w1, w31
+
+  /* Trigger a fault if FG0.Z is false. */
+  csrrs     x2, 0x7c0, x0
+  srli      x2, x2, 3
+  andi      x2, x2, 1
+  bne       x2, x0, _pk_valid
+  unimp
 
 
 .data
