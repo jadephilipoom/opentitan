d2cd87b5979f7964bf42fb796b2baf5a5f96949d
Jade Philipoom <jadep@google.com>
2023-12-22 17:29:00 +0000

    getting there, have scripts to make test data and run benchmark, but getting errors

---
+++ make_test_data.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Generates test data for different sets of SPHINCS+ parameters.
+#
+# Edits the Makefile in the SPHINCS+ reference implementation to point to a
+# different parameter set, generates test vectors, and then restores the
+# previous state. The parameter set should be a C header file whose format is
+# compatible with the reference implementation.
+#
+# The $SPX_REPO_TOP environment variable should be set to the root of the
+# `sphincsplus` repo.
+
+set -e;
+USAGE="USAGE: $0 PARAMS_HEADER TEST_DST"
+
+if [ -z "$SPX_REPO_TOP" ]
+then
+  echo "\$SPX_REPO_TOP is not defined! It should be set to the location of the sphincsplus repo root.";
+  exit 1;
+fi
+
+if [ -z "$1" ] | [ -z "$2" ]
+then
+  echo $USAGE;
+  exit 1;
+fi
+
+HEADER=$(realpath $1);
+TEST_DST=$(realpath $2);
+PARAMS_DIR=$SPX_REPO_TOP/ref/params;
+PARAMS_FILENAME=$(basename $HEADER);
+PARAMS_NAME="${PARAMS_FILENAME%.*}"
+
+if [ -f "$TEST_DST" ]
+then
+  echo "Destination file $TEST_DST already exists!";
+  exit 1;
+fi
+
+echo "Generating test data for $PARAMS_NAME, storing in $TEST_DST";
+
+cd $SPX_REPO_TOP/ref;
+cp $HEADER $PARAMS_DIR;
+sed -i -e "s/PARAMS = .*/PARAMS = $PARAMS_NAME/" Makefile;
+sed -i -e "s/THASH = .*/THASH = simple/" Makefile;
+make && ./PQCgenKAT_sign;
+git restore Makefile;
+cp PQCsignKAT_64.rsp $TEST_DST;
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -4,15 +4,12 @@
 # directory. Saves the test logs in the log directory.
 #
 # The $OT_REPO_TOP environment variable should be set to the root of the
-# `opentitan` repo.
-#
-# USAGE: ./run_benchmark.sh <params directory> <log directory>
+# `opentitan` repo. Likewise, the $SPX_REPO_TOP environment variable should be
+# set to the root of the `sphincsplus` repo.
 
 set -e;
 
-HEADER_DIR=$PWD/$1;
-LOG_DIR=$PWD/$2;
-BENCHMARK_PARAMS_DST="$OT_REPO_TOP/sw/device/silicon_creator/lib/sigverify/sphincsplus/params/benchmark_params.h";
+USAGE="USAGE: $0 PARAMS_DIR LOG_DIR TEST_DIR";
 
 if [ -z "$OT_REPO_TOP" ]
 then
@@ -20,13 +17,44 @@ then
   exit 1;
 fi
 
-echo "Reading headers from $HEADER_DIR/ and saving logs to $LOG_DIR/";
+if [ -z "$SPX_REPO_TOP" ]
+then
+  echo "\$SPX_REPO_TOP is not defined! It should be set to the location of the sphincsplus repo root.";
+  exit 1;
+fi
+
+if [ -z "$1" ] | [ -z "$2" ] | [ -z "$3" ]
+then
+  echo $USAGE;
+  exit 1;
+fi
+
+HEADER_DIR=$(realpath $1);
+LOG_DIR=$(realpath $2);
+TEST_DIR=$(realpath $3);
+KAT_DIR=$OT_REPO_TOP/sw/device/tests/crypto/testvectors/sphincsplus_kat;
+BENCHMARK_PARAMS_DST="$OT_REPO_TOP/sw/device/silicon_creator/lib/sigverify/sphincsplus/params/benchmark_params.h";
+
+
+echo "Reading headers from $HEADER_DIR/ and tests from $TEST_DIR/";
+echo "Saving logs to $LOG_DIR/";
 
 cd $OT_REPO_TOP;
 for header in "$HEADER_DIR/*"
 do
-  name=$(basename $header);
-  echo "Running benchmark for $name...";
+  NAME=$(basename $header);
+  echo "Running benchmark for $NAME...";
+
+  # Look for a KAT .rsp file (using the naming scheme from make_test_data.sh)
+  # and run the test-parsing script. If the .rsp file doesn't exist, create it.
+  RSP=$TEST_DIR/KAT_$NAME.rsp;
+  if ! [ -f "$RSP" ]
+  then
+    ./make_test_data.sh $header $RSP;
+  fi
+  $KAT_DIR/parse_kat.py --num-tests=10 $RSP $KAT_DIR/sphincsplus_testvectors_kat.hjson;
+
+  # Copy the header into the benchmarks and run the test.
   cp $header $BENCHMARK_PARAMS_DST;
-  ./bazelisk.sh test --test_output=streamed //sw/device/silicon_creator/lib/sigverify/sphincsplus/test:verify_test_hardcoded_sim_verilator > $LOG_DIR/out_$name.log 2>$LOG_DIR/err_$name.log;
+  ./bazelisk.sh test --test_output=streamed //sw/device/silicon_creator/lib/sigverify/sphincsplus/test:verify_test_kat0_sim_verilator > $LOG_DIR/out_$name.log 2>$LOG_DIR/err_$name.log;
 done
