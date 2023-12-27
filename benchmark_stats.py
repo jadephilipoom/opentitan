ddf0817d99d5d7467a526f75b4a4930cc6866dfc
Jade Philipoom <jadep@google.com>
2023-12-27 15:58:11 +0000

    add a script to parse statistics

--- benchmark_stats.py
+++ benchmark_stats.py
@@ -11,29 +11,25 @@ VERIFY_CYCLES_PATTERN = re.compile('Verification took ([0-9]+) cycles.')
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
       description='Extract signature verification cycle counts from a log file and print some statistics about them.')
+  # Note: encoding errors are ignored when reading from the log file because
+  # Verilator prints out carriage returns that cause issues if not ignored.
   parser.add_argument('log',
                       metavar='FILE',
-                      type=argparse.FileType('r'),
+                      type=argparse.FileType('r', errors='ignore'),
                       help='Log file to read.')
   args = parser.parse_args()
 
   # Read the log and find the cycle counts.
   with args.log as log:
-    log_content = template.read()
-  matches = re.findall(VERIFY_CYCLES_PATTERN, log_content)
+    matches = re.findall(VERIFY_CYCLES_PATTERN, log.read())
   if not matches:
     raise ValueError(f'No cycle counts found! Did the benchmark complete successfully?')
 
   # Interpret the cycle counts as integers.
-  cycles = []
-  for m in matches:
-    cycles_str = m.group(1)
-    if not cycles_str.isdecimal():
-      raise ValueError(f'Cannot interpret cycle count {cycles_str} from regex match {m.group(0)}.')
-    cycles.append(int(cycles_str))
+  cycles = [int(m) for m in matches]
 
   # Print some helpful statistics.
   print(f'Number of tests: {len(cycles)}')
   print(f'Min cycle count: {min(cycles)}')
   print(f'Max cycle count: {max(cycles)}')
-  print(f'Avg cycle count: {sum(cycles) / len(cycles)}')
+  print(f'Avg cycle count: {round(sum(cycles) / len(cycles))}')
