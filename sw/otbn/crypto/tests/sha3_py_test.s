b262606256adb0efa4cca10905160b19db663c70
Amin Abdulrahman <mail@d0pamin.de>
2024-02-08 18:35:50 +0100

    SHAKE python asm test working

--- sw/otbn/crypto/tests/sha3_py_test.s
+++ sw/otbn/crypto/tests/sha3_py_test.s
@@ -11,65 +11,60 @@ run_sha3:
   /* Load stack pointer */
   la x2, stack_end
 
-  /* SHA3-224, corner case with 0-length message */
-
   la x10, context
-  li x11, 28 /* mdlen */
+  /* Get digest size in bytes */
+  la x5, dgst_size
+  lw x5, 0(x5)
+  srli x11, x5, 3
   jal x1, sha3_init
 
+  /* SHA or SHAKE? */
+  la x5, dgst_mode
+  lw x5, 0(x5)
+  bne x5, zero, _SHAKE
+
+_SHA:
   la x10, context
-  la x11, test_msg_256_32
-  li x12, 0 /* msglen */
+  la x11, msg
+  la x12, msg_len
+  lw x12, 0(x12)
   jal x1, sha3_update
 
   la x10, context
-  la x11, test_dst_224
+  la x11, digest
   jal x1, sha3_final
-
-  /* SHA3-256, short message */
-
-  la x10, context
-  li x11, 32 /* mdlen */
-  jal x1, sha3_init
-
-  la x10, context
-  la x11, test_msg_256_32
-  li x12, 32 /* msglen */
-  jal x1, sha3_update
-
-  la x10, context
-  la x11, test_dst_256
-  jal x1, sha3_final
-
-  /* SHA3-384, exact block size */
+  beq zero, zero, _END
+_SHAKE:
+  la x19, context
+  la x18, msg
+  la x9, shake_absorb_size
+  lw x9, 0(x9)
+  la x8, shake_absorbs
+  lw x8, 0(x8)
+  LOOP x8, 5
+    addi x10, x19, 0  /* Copy back context address */
+    addi x11, x18, 0  /* Copy back message address */
+    addi x12, x9, 0   /* Copy back the absorb size */
+    jal  x1, sha3_update
+    add  x18, x18, zero /* Advance message address */
 
   la x10, context
-  li x11, 48 /* mdlen */
-  jal x1, sha3_init
+  jal x1, shake_xof
 
-  la x10, context
-  la x11, test_msg_384_104
-  li x12, 104 /* msglen */
-  jal x1, sha3_update
+  la x8, context
+  la x9, digest
+  la x18, shake_squeezes
+  lw x18, 0(x18)
+  la x19, shake_squeeze_size
+  lw x19, 0(x19)
+  LOOP x18, 5
+    addi x10, x8, 0  /* Copy back context */
+    addi x11, x9, 0  /* Copy back digest address */
+    addi x12, x19, 0 /* Copy back squeeze size */
+    jal x1, shake_out
+    add x9, x9, zero  /* Advance digest address */
 
-  la x10, context
-  la x11, test_dst_384
-  jal x1, sha3_final
-
-  /* SHA3-512, multiblock message */
-
-  la x10, context
-  li x11, 64 /* mdlen */
-  jal x1, sha3_init
-
-  la x10, context
-  la x11, test_msg_512_255
-  li x12, 255 /* msglen */
-  jal x1, sha3_update
-
-  la x10, context
-  la x11, test_dst_512
-  jal x1, sha3_final
+_END:
 
   ecall
 
@@ -78,8 +73,11 @@ run_sha3:
 .global msg
 msg:
   .balign 32
-  .zero 1024
-
+  .dword 0xA3A3A3A3A3A3A3A3
+  .dword 0xA3A3A3A3A3A3A3A3
+  .dword 0xA3A3A3A3
+  .zero 1004 /* 1024! */
+/* digest starts at 1028! Not clear why. */
 .global digest
 digest:
   .balign 32
@@ -93,13 +91,34 @@ msg_len:
 .global dgst_mode /* 0=SHA-3, 1=SHAKE */
 dgst_mode:
   .balign 4
-  .zero 4
+  .word 0x00000001
 
 .global dgst_size /* Desired digest size/length for SHAKE */
-dgst_mode:
+dgst_size:
   .balign 4
-  .zero 4
+  .word 0x00000080
 
+.global shake_absorbs /* Number of times data is absorbed */
+shake_absorbs:
+  .balign 4
+  .word 0x0000000A
+
+.global shake_absorb_size /* Amount of bytes to be absorbed each time */
+shake_absorb_size:
+  .balign 4
+  .word 0x00000014
+
+.global shake_squeezes /* Number of times data is squeezed */
+shake_squeezes:
+  .balign 4
+  .word 0x00000010
+
+.global shake_squeeze_size /* Number of bytes to be squeezed each time */
+shake_squeeze_size:
+  .balign 4
+  .word 0x00000020
+
+.balign 32
 .global context
 context:
   .balign 32
